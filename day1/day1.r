c25a41d020d696e87a88bbb0977581595db8afaf
Gordon Lin <contact@gordonlin.ca>
2024-12-02 19:45:56 -0500

    Added part 2 solutions for day 1

--- day1/day1.r
+++ day1/day1.r
@@ -1,10 +1,22 @@
 input <- read.table("input", header = FALSE)
 
 day_1_part_1 <- function(input) {
-    input$V1 <- sort(as.numeric(input$V1))
-    input$V2 <- sort(as.numeric(input$V2))
+    input$V1 <- sort(input$V1)
+    input$V2 <- sort(input$V2)
     diff <- abs(input$V1 - input$V2)
     sum(diff)
 }
 
 day_1_part_1(input)
+
+day_1_part_2 <- function(input) {
+    freq_table <- table(input$V2)
+    stringed_input <- as.character(input$V1)
+    freq <- freq_table[stringed_input]
+    freq[which(is.na(freq))] <- 0
+    sum(
+        input$V1 * freq
+    )
+}
+
+day_1_part_2(input)
