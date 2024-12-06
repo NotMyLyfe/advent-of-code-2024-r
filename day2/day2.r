5d89a116ca0f24516e909e74d706a81944eaf3e2
Gordon Lin <contact@gordonlin.ca>
2024-12-05 22:59:53 -0500

    Completed part 2 of day 2

--- day2/day2.r
+++ day2/day2.r
@@ -1,5 +1,11 @@
 input <- read.table("input", header = FALSE, fill = TRUE)
 
+safety_check <- function(arr) {
+    length(unique(sign(arr))) == 1 &&
+        max(abs(arr)) <= 3 &&
+        min(abs(arr)) >= 1
+}
+
 day_2_part_1 <- function(input) {
     mat <- as.matrix(input)
     sum(apply(
@@ -9,11 +15,41 @@ day_2_part_1 <- function(input) {
             x <- x[!is.na(x)]
             pair_dif <- x[2:length(x)] - x[1:(length(x) - 1)]
 
-            (length(unique(sign(pair_dif))) == 1 &&
-                max(abs(pair_dif)) <= 3 &&
-                min(abs(pair_dif)) >= 1)
+            safety_check(pair_dif)
         }
     ))
 }
 
 day_2_part_1(input)
+
+day_2_part_2 <- function(input) {
+    mat <- as.matrix(input)
+    sum(apply(
+        X = mat,
+        MARGIN = 1,
+        FUN = function(x) {
+            x <- x[!is.na(x)]
+            pair_dif <- x[2:length(x)] - x[1:(length(x) - 1)]
+
+            if (safety_check(pair_dif) ||
+                safety_check(pair_dif[-1]) ||
+                safety_check(pair_dif[-length(pair_dif)])) {
+                return(1)
+            }
+
+            for (i in 2:(length(x) - 1)) {
+                skipped_dif <- x[i + 1] - x[i - 1]
+                dif_list <- c(
+                    pair_dif[-c(i, i - 1)],
+                    skipped_dif
+                )
+                if (safety_check(dif_list)) {
+                    return(1)
+                }
+            }
+            return(0)
+        }
+    ))
+}
+
+day_2_part_2(input)
